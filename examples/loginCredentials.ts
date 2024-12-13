4ffff5882bf2f390da709dd409b549bf78d77594
Rémy Godet <godetremy@free.fr>
2024-12-13 16:23:37 +0100

    feat(login): Implement login

---
+++ examples/loginCredentials.ts
@@ -0,0 +1,9 @@
+import * as alise from "../src";
+import { credentials } from "./_credentials";
+
+void async function main () {
+    const session = await alise.loginCredentials(credentials.baseURL!, credentials.username!, credentials.password!);
+    // you're now authenticated with the session !
+    // you can now use the session to make further requests.
+    console.log("You're authenticated with the session", session.id);
+}();
