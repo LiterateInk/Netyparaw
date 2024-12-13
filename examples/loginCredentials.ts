26420e60010cbd8094bc582082c0997aa33f886d
Rémy Godet <godetremy@free.fr>
2024-12-13 16:47:22 +0100

    fix(example): Replaced alise with netypareo

--- examples/loginCredentials.ts
+++ examples/loginCredentials.ts
@@ -1,8 +1,8 @@
-import * as alise from "../src";
+import * as netypareo from "../src";
 import { credentials } from "./_credentials";
 
 void async function main () {
-    const session = await alise.loginCredentials(credentials.baseURL!, credentials.username!, credentials.password!);
+    const session = await netypareo.loginCredentials(credentials.baseURL!, credentials.username!, credentials.password!);
     // you're now authenticated with the session !
     // you can now use the session to make further requests.
     console.log("You're authenticated with the session", session.id);
